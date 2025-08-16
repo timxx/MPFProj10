3f22ca5aa9bc35f9fa794a5b2a6c836cad2d1755
Weitian Leung <weitianleung@gmail.com>
2025-08-16 17:01:26 +0800

    Add support for Visual Studio 2019

--- Dev10/Src/CSharp/Properties/AssemblyInfo.cs
+++ Dev10/Src/CSharp/Properties/AssemblyInfo.cs
@@ -17,6 +17,10 @@ using System.Resources;
 [assembly: AssemblyTitle("Microsoft.VisualStudio.Project.12.0")]
 [assembly: AssemblyProduct("Microsoft.VisualStudio.Project.12.0")]
 [assembly: Guid("b746a4b5-43e2-49f8-9a76-828ad3a96142")]
+#elif DEV16
+[assembly: AssemblyTitle("Microsoft.VisualStudio.Project.16.0")]
+[assembly: AssemblyProduct("Microsoft.VisualStudio.Project.16.0")]
+[assembly: Guid("a6a9cb31-3c78-45e8-9892-99cf03c62a90")]
 #else
 #error Unknown target.
 #endif
